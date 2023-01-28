bf2ece9e95033a77daf5677a6d3be8793be47667
Jakub <jakub.bytow@gmail.com>
2023-01-28 17:16:17 +0100

    0.5 converter works

--- main.py
+++ main.py
@@ -25,4 +25,21 @@ def get_yearly_rates(amount, currency, converted_currency, amount_of_days):
         current_date = item
         currency_rate = data['rates'][item][converted_currency]
 
+        currency_history[current_date] = [currency_rate]
+        rate_history_array.append(currency_rate)
 
+    # Clean Data
+    pd_data = pd.DataFrame(currency_history).transpose()
+    pd_data.columns = ['Rate']
+    pd.set_option('display.max.rows', None)
+    print(pd_data)
+
+    #Plot Data
+    plt.plot(rate_history_array)
+    plt.ylabel(f'{amount} {currency} to {converted_currency}')
+    plt.xlabel('Days')
+    plt.title(f'Current rate for {amount} {currency} to {converted_currency} is {rate_history_array[-1]}')
+    plt.show()
+
+
+get_yearly_rates(1, 'EUR', 'GBP', 365)
